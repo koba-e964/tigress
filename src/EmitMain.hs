5a0526384075cbdbc2e078252a00aabca04df020
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-05-23 18:21:24 +0900

    Fix EmitMain.hs (emit to file)
    "rlwrap cabal run tigress-emit -- -o test.ll" will write output to test.ll

--- src/EmitMain.hs
+++ src/EmitMain.hs
@@ -4,7 +4,7 @@ import LLVM.General.Module
 
 import Data.List (foldl')
 import Control.Monad.Except
-import Control.Monad.State (StateT, evalStateT, runStateT)
+import Control.Monad.State (StateT, evalStateT)
 import System.IO
 import System.Environment (getArgs)
 import System.Console.GetOpt
@@ -43,40 +43,57 @@ main = do
   unless (null err) $ error $ "err:" ++ show err 
   let conf = foldl' (.) id ops defaultConf -- currently not used
   case rest of
-    [] -> evalStateT repl conf
+    [] -> do
+      case outFile conf of
+        Nothing -> repl
+        Just path -> do
+          handle <- openFile path WriteMode
+          line <- getLine
+          liftError $ interpretString line handle False
+          hClose handle
     (_name : _) -> error "TODO:source file"
-repl :: StateT Config IO ()
+repl :: IO ()
 repl = do
     liftIO $ hSetBuffering stdout NoBuffering
     liftIO $ putStr "> "
     line <- liftIO $ getLine
-    let toks = TL.alexScanTokens line
+    liftError $ interpretString line stdout True
+    repl
+-- compiles expr and displays the LLVM code and the result.
+-- if 'isStdOut' is True, the result will be written to the stdout.
+-- otherwise, the result will be written to 'handle'.
+interpretString :: String -> Handle -> Bool -> ExceptT String IO ()
+interpretString str handle isStdOut = do
+    let toks = TL.alexScanTokens str
     let exprOrErr = TP.tparse toks
     case exprOrErr of
-      Left err -> liftIO (print err)
+      Left err -> liftIO (hPutStrLn stderr err)
       Right expr -> do
         liftIO $ do
-          x <- runExceptT $ interpretExpr expr
+          x <- runExceptT $ interpretExpr expr handle isStdOut
           case x of
             Left err -> hPutStrLn stderr err
             Right _ -> return ()
-    repl
-  where
-    -- compiles expr and displays the LLVM code and the result.
-    interpretExpr :: Expr -> ExceptT String IO ()
-    interpretExpr expr = do
-         liftIO $ print expr
-         newmod <- liftEither $ codegen (emptyModule "JITtest") [expr]
-         optmod <- optimize newmod (Just 3)
-         withContextT $ \ctx -> do
-           withModuleFromAST ctx newmod $ \mm -> do
-             putStrLn "***** Module before optimization *****"
-             s <- moduleLLVMAssembly mm
-             putStrLn s
-           withModuleFromAST ctx optmod $ \mm -> do
-             putStrLn "***** Optimized Module *****"
-             s <- moduleLLVMAssembly mm
-             putStrLn s
-         result <- runJIT optmod
-         liftIO $ putStrLn ("result = " ++ show result)
+
+interpretExpr :: Expr -> Handle -> Bool -> ExceptT String IO ()
+interpretExpr expr handle isStdOut = do
+  liftIO $ print expr
+  newmod <- liftEither $ codegen (emptyModule "JITtest") [expr]
+  optmod <- optimize newmod (Just 3)
+  withContextT $ \ctx -> do
+   if isStdOut then do
+     withModuleFromAST ctx newmod $ \mm -> do
+       putStrLn "***** Module before optimization *****"
+       s <- moduleLLVMAssembly mm
+       putStrLn s
+     withModuleFromAST ctx optmod $ \mm -> do
+       putStrLn "***** Optimized Module *****"
+       s <- moduleLLVMAssembly mm
+       putStrLn s
+   else
+     withModuleFromAST ctx optmod $ \mm -> do
+       s <- moduleLLVMAssembly mm
+       hPutStr handle s
+  result <- runJIT optmod
+  liftIO $ putStrLn ("result = " ++ show result)
 
